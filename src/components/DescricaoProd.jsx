046bcfff225b9bb61ef3f6a7ab227eb4946b1558
juliordg <juliordg@gmail.com>
2025-05-22 08:55:06 -0300

    criação da pagiande deswcrição e seus estilops

---
+++ src/components/DescricaoProd.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './DescricaoProd.module.css'
+
+const DescricaoProd = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DescricaoProd
---
+++ src/pages/DescriProdutos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DescriProdutos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DescriProdutos
