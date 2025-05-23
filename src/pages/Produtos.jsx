f90eed0ebb38df01d40f916c92acbaf40e8a48ea
Otávio Levi <otavio.holanda05@gmail.com>
2025-05-23 09:24:21 -0300

    Adicionando CardProd e FiltroProd para Produtos

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Ordenar from '../components/Ordenar'
+import FiltroProd from '../components/FiltroProd'
+import CardProd from '../components/CardProd'
 
 const Produtos = () => {
   return (
-    <>
+    <div style={{backgroundColor: "#f9f8fe"}} >
       <Ordenar />
-    </>
+      <div className='d-flex'>
+      <FiltroProd />
+      <CardProd />
+      </div>
+    </div>
   )
 }
 
