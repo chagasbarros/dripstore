a927ec8bce253d406d14699ab7cfbd3c21f6a153
chagasbarros <chagasbarros@outlook.com>
2025-06-04 15:22:05 -0300

    subindo a página Criar conta

---
+++ src/pages/CadastreSe.jsx
@@ -0,0 +1,11 @@
+import CriarConta from "../components/CriarConta"
+
+const CadastreSe = () => {
+    return (
+        <>
+            <CriarConta/>
+        </>
+    )
+}
+
+export default CadastreSe;
