4aa26197d759e13dc9bd52becb54749d00ffbf09
chagasbarros <chagasbarros@outlook.com>
2025-06-17 11:48:53 -0300

    conclui html da página perfil

--- frontend/src/components/PerfilPagamento.jsx
+++ frontend/src/components/PerfilPagamento.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import FormaPagamento from "./FormaPagamento";
+
 const PerfilPagamento = ({perfil}) => {
+
+  const [pagamentoNovo, setPagamentoNovo] = useState('pagamentoExistente')
+
+  const adicionarPagamento = ()=> {setPagamentoNovo('pagamentoNovo')}
+  const cancelarPagamento = ()=> {setPagamentoNovo('pagamentoExistente')}
     return (
-      <section className="bg-white p-5 col-8">
-        <div className="d-flex justify-content-between">
+      <section className="bg-white col-8">
+
+        {pagamentoNovo === 'pagamentoExistente' && (
+          <div>
+            <div className="d-flex justify-content-between">
           <h6>Métodos de pagamento</h6>
           <button
             className="btn btn-pink text-white fw-bold"
             style={{ backgroundColor: "#d10f7d" }}
+            onClick={adicionarPagamento}
           >
             Adicionar
           </button>
@@ -20,9 +32,26 @@ const PerfilPagamento = ({perfil}) => {
                 Final do Cartão: **** **** ****{" "}
                 {perfil[0].numerosCartoes.split(" | ")[index].slice(-4)}
               </li>
+              <button className="btn btn-dark" >excluir</button>
             </ul>
+            
           ))}
         </div>
+          </div>
+        )}
+
+        {pagamentoNovo === 'pagamentoNovo' && (
+          <div>
+            <div>
+              <FormaPagamento/>
+            </div>
+             <div className="d-flex gap-3 justify-content-end me-5">
+              <button className="btn btn-warning" onClick={cancelarPagamento}>Cancelar</button>
+              <button className="btn btn-success">Salvar</button>
+            </div>
+          </div>
+        )}
+        
       </section>
     );
         
--- frontend/src/components/PerfilPessoal.jsx
+++ frontend/src/components/PerfilPessoal.jsx
@@ -1,45 +1,43 @@
 import { useState } from "react"
 
 const PerfilPessoal = ({perfil}) => {
-  const [editar, setEditar] = useState(perfil)
-  const idPerfil = 8
-  const [modoEdicao, setModoEdicao] = useState("salvar")
+  // const [editar, setEditar] = useState(perfil)
+  // const idPerfil = 8
+  const [modoEdicao, setModoEdicao] = useState("editar")
 
-  
+  const editarInformacoes = () => {
+    setModoEdicao('salvar')
+  }
 
-    
-
-
-
-    return <div>
+  const cancelarEdicao  = () => setModoEdicao("editar")
+    return (
+      <div>
         <section className="bg-white p-5 col-8">
+
+          {modoEdicao === "editar" && (
+            <div>
               <div className="d-flex justify-content-between">
                 <h5>Minhas Informações</h5>
                 <button
                   className="btn btn-pink text-white fw-bold"
                   style={{ backgroundColor: "#d10f7d" }}
-
+                  onClick={editarInformacoes}
                 >
-                editar
+                  editar
                 </button>
               </div>
-              <hr />
-
-              {modoEdicao === "editar" && (
-                <div>
-                  <div>
+              <div>
                 <h5>Informações Pessoais</h5>
                 <ul className="navbar-nav">
                   <li>Nome: {perfil[0].nome}</li>
-                  
+
                   <li>CPF: {perfil[0].cpf_cnpj}</li>
-                  
+
                   <li>Email: {perfil[0].email}</li>
-                  
+
                   <li>Celular: {perfil[0].telefone}</li>
-                  
                 </ul>
-                
+
                 <hr />
               </div>
 
@@ -52,45 +50,104 @@ const PerfilPessoal = ({perfil}) => {
                   <li>CEP: {perfil[0].cep}</li>
                 </ul>
               </div>
-                </div>
-              )}
+            </div>
+          )}
 
-              {modoEdicao === "salvar" && (
-                <div>
-                  <label htmlFor="name" className="form-label">Nome:</label>
-                  <input type="text" name="name" value={onchange} className="form-control " />
-                
-                  <label htmlFor="cpf">CPF:</label>
-                  <input type="text" name="cpf" value={onchange} className="form-control" />
+          {modoEdicao === "salvar" && (
+            <div>
+              <div className="d-flex justify-content-between">
 
-                  <label htmlFor="email">Email:</label>
-                  <input type="text" name="email" value={onchange} className="form-control" />
+                <h5>Alterar Informações</h5>
 
-                  <label htmlFor="celular">Celular:</label>
-                  <input type="text" name="celular" value={onchange} className="form-control" />
+                <button className="btn btn-warning text-white fw-bold" onClick={cancelarEdicao}>
+                  Cancelar
+                </button>
 
-                  
+                <button className="btn btn-success text-white fw-bold">
+                  Salvar
+                </button>
+              </div>
+              <hr />
+              <label htmlFor="name" className="form-label">
+                Nome:
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={onchange}
+                className="form-control "
+                placeholder={perfil[0].nome}
+              />
 
-                <h5 className="mt-5">Informações de Entrega</h5>
-                <hr className="my-4" />
-                  <label htmlFor="endereco">Endereço</label>
-                  <input type="text" name="endereco" value={onchange} className="form-control" />
+              <label htmlFor="cpf">CPF:</label>
+              <input
+                type="text"
+                name="cpf"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].cpf_cnpj}
+              />
 
-                  <label htmlFor="bairro">Bairro:</label>
-                  <input type="text" name="bairro" value={onchange} className="form-control" />
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                name="email"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].email}
+              />
 
-                  <label htmlFor="cidade">Cidade:</label>
-                  <input type="text" name="cidade" value={onchange} className="form-control" />
+              <label htmlFor="celular">Celular:</label>
+              <input
+                type="text"
+                name="celular"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].telefone}
+              />
 
-                  <label htmlFor="cep">CEP:</label>
-                  <input type="text" name="cep" value={onchange} className="form-control" />
+              <h5 className="mt-5">Informações de Entrega</h5>
+              <hr className="my-4" />
+              <label htmlFor="endereco">Endereço</label>
+              <input
+                type="text"
+                name="endereco"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].rua}
+              />
 
+              <label htmlFor="bairro">Bairro:</label>
+              <input
+                type="text"
+                name="bairro"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].bairro}
+              />
 
-                </div>
-              )}
-              
-            </section>
-    </div>
+              <label htmlFor="cidade">Cidade:</label>
+              <input
+                type="text"
+                name="cidade"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].cidade}
+              />
+
+              <label htmlFor="cep">CEP:</label>
+              <input
+                type="text"
+                name="cep"
+                value={onchange}
+                className="form-control"
+                placeholder={perfil[0].cep}
+              />
+            </div>
+          )}
+        </section>
+      </div>
+    );
 }
 
 export default PerfilPessoal
